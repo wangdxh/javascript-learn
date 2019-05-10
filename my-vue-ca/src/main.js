8cfe44adb7775408437623524d27ea159c5a8f96
wangdxh <wangdxh@163.com>
2019-05-10 11:23:22 +0800

    good

--- my-vue-ca/src/main.js
+++ my-vue-ca/src/main.js
@@ -26,6 +26,20 @@ Vue.component('canvasrect', canvasrect)
 import cameravideo from '@/components/Cameravideo'
 Vue.component('cameravideo', cameravideo)
 
+// 高德
+// 引入vue-amap
+import VueAMap from 'vue-amap';
+Vue.use(VueAMap);
+
+// 初始化vue-amap
+VueAMap.initAMapApiLoader({
+  // 高德的key
+  key: '160cab8ad6c50752175d76e61ef92c50',
+  // 插件集合
+  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor'],
+  // 高德 sdk 版本，默认为 1.4.4
+  v: '1.4.4'
+});
 
 Vue.config.productionTip = true;
 
--- my-vue-ca/src/views/dashboard/index.vue
+++ my-vue-ca/src/views/dashboard/index.vue
@@ -1,5 +1,83 @@
 <template>
     <div>
         <h3>dashboard</h3>
+        <br>
+        <div style="width=100%;height:400px">
+                 
+            <el-amap
+                ref="map"
+                vid="amapDemo"
+                :center="center"
+                :zoom="zoom">
+                <!-- 在地图上标记点 -->
+                <el-amap-marker
+                v-for="(marker,index) in markers"
+                :key="index"
+                :position="marker" >
+                </el-amap-marker>
+                <!-- 坐标点 -->
+                <el-amap-marker vid="amapDemo" :position="center"></el-amap-marker>
+                
+                <!-- 放置图片 -->
+                <el-amap-ground-image v-for="(groundimage,index) in groundimages"
+                :url="groundimage.url"
+                :key="index"
+                :bounds="groundimage.bounds"
+                :events="groundimage.events">
+                </el-amap-ground-image>
+                
+                 <el-amap-text v-for="(text,index) in texts"
+                :text="text.text"
+                :key="index"
+                :offset="text.offset"
+                :position="text.position"
+                :events="text.events">
+                </el-amap-text>
+            </el-amap>
+            
+        </div>
+        
     </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      zoom: 10,
+      center: [121.59996, 31.197646], // 中心位置
+      markers: [
+        [121.49996, 31.297646],
+        [121.40018, 31.197622],
+        [121.49991, 31.207649]
+      ],
+      groundimages: [
+        /*{
+          url:
+            "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif?imageView2/1/w/80/h/80",
+          bounds: [[121.5273285, 31.21515044], [122.9273285, 32.31515044]],
+          events: {
+            click() {
+              alert("click groundimage");
+            }
+          }
+        }*/
+      ],
+      texts: [
+        {
+          position: [121.5273285, 31.27515044],
+          text: `<b>this is test from <br> Mr wangxiaohui</b>`,
+          offset: [0, 0],
+          events: {
+            click: () => {
+              //alert('click text')
+            }
+          }
+        }
+      ],
+
+    };
+  }
+};
+//from https://blog.csdn.net/qq_24147051/article/details/84763265
+</script>
